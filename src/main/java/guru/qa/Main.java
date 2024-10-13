3d4d6d9c4ba0372068150d427a27e57b35dfe67b
Maksim Skomorokhov <53369027+null-nvrsk@users.noreply.github.com>
2024-10-13 18:47:54 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,10 @@
 package guru.qa;
 
 public class Main {
+
+    private static msg = "Hello Java! (Edited in GitHub)";
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(msg);
     }
 }
